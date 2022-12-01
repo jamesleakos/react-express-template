d11e9f84ca2f8031e7a99d71ee85c20f559d8e14
James Leakos <jamesleakos@gmail.com>
2022-12-01 00:02:03 -0700

    basic express / react template

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      James Leakos
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,8 @@
+const path = require("path")
+const express = require("express"); // npm installed
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.listen(3000);
